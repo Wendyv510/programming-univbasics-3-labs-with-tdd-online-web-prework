bc64d8a67dceb3e6b6cf6a1139a668def6ef6f34
Wendy Harris <wendyv510@hotmail.com>
2019-09-19 20:29:59 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -30,3 +30,11 @@ it "contains a local variable called sum that is assigned to the result of addin
   
   expect (sum). to equal (first_number + second_number)
 end
+
+it "contains a local variable called difference that is assigned to the result of subtracting first_number and second_number" do
+  first_number = get_variable_from_file ('./calculator.rb', "first_number")
+  second_number = get_variable_from_file ('./calculator.rb', "second_number")
+  difference = get_variable_from_file ('./calculator.rb', "difference")
+  
+  expect (difference). to equal (first_number - second_number)
+end
