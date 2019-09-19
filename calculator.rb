00cc2c3cd8659942a6ccb2c81fc6e768bd52f2f6
Wendy Harris <wendyv510@hotmail.com>
2019-09-19 20:40:26 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -47,3 +47,10 @@ it "contains a local variable called product that is assigned to the result of m
   expect (product). to equal (first_number * second_number)
 end
 
+it "contains a local variable called quotient that is assigned to the result of dividing first_number and second_number" do
+  first_number = get_variable_from_file ('./calculator.rb', "first_number")
+  second_number = get_variable_from_file ('./calculator.rb', "second_number")
+  quotient = get_variable_from_file ('./calculator.rb', "quotient")
+  
+  expect (quotient). to equal (first_number/second_number)
+end
